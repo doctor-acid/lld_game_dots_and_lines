fe8845ec54af5375b0f0c94d91883b0d38362c78
doctor-acid <avnishbadoni@gmail.com>
2023-11-01 13:02:48 +0530

    added autoMove for testing; input functional

--- src/index.ts
+++ src/index.ts
@@ -18,23 +18,16 @@ try {
     let numberOfPlayers;
 
     e="test";
-    while(e){
-        console.log(e);
+    while(e!=null){
+        // console.log(e);
         try {
-            sizeOfBoard = prompt('How many rows-columns of boxes do you want in the game? ');
-            numberOfPlayers = prompt('How many players will play the game. Enter a number between 2 and 5?');
-            if(!isNaN(numberOfPlayers) && !isFinite(numberOfPlayers)){
-                e="Please enter a valid numeric value for number of players";
-            }
-            if(numberOfPlayers<2 || numberOfPlayers>5){
-                e="Please enter a value between 2 and 5 for the number of players"
-            }
-            e=null;
+            ({sizeOfBoard, numberOfPlayers, e} = setGameParams(sizeOfBoard, numberOfPlayers, e));
         } catch (error) {
+            console.log(error)
             e=error;
         }
         // console.error(chalk.red(e));
-        console.log(e)
+        
     }
     
     // let sizeOfBoard = 5;
@@ -46,7 +39,7 @@ try {
     // console.log(`Number of players = ${numberOfPlayers}`);
 
     const gameBuilder = new GameBuilder();
-    gameBuilder.setBoxDimension(sizeOfBoard);
+    gameBuilder.setBoxDimension(sizeOfBoard!);
 
     let playerList = [];
     // playerList.push(new Player("Vivek", "v"))
@@ -56,7 +49,7 @@ try {
     e=null;
 
     //ADD PLAYERS
-    while(!(i>=numberOfPlayers && !e)){
+    while(!(i>=numberOfPlayers! && !e)){
         try {
             const p: string = prompt('Enter the name of player '+(i+1));
             // console.log(`Number of players = ${numberOfPlayers}`);
@@ -85,6 +78,15 @@ try {
     i=0;
     e=null;
 
+    // GAME TEST VARS;
+    let testPoint1 = 0;
+    let testPoint2 = 0;
+    let moveSideA;
+    let moveSideB;
+    let d1: string|null, d2: string|null;
+
+    let autoMove = true;
+
     // START GAME
     gameController.setGameState(GameState.IN_PROGRESS);
     while(game.gameState===GameState.IN_PROGRESS || game.gameState===GameState.NOT_STARTED){
@@ -92,15 +94,24 @@ try {
         try {
             gameController.displayBoard();
             let thisPlayer = playerList[game.playerMoveOrder![i%playerList.length]];
-            const d1: string = prompt(thisPlayer.name+"'s move. Please enter the dot number of first dot to draw a line from");
-            const d2: string = prompt("Enter the dot number of second dot to draw a line to. Choose adjacent dot only");
-            i = gameController.move(d1, d2, thisPlayer, i);
+            if(!autoMove){
+                d1 = prompt(thisPlayer.name+"'s move. Please enter the dot number of first dot to draw a line from");
+                d2 = prompt("Enter the dot number of second dot to draw a line to. Choose adjacent dot only");
+            }else{
+                const s = prompt("press space for next move");
+                ({d1, d2, testPoint1, testPoint2, moveSideA, moveSideB} = testGame(testPoint1, testPoint2, sizeOfBoard!, moveSideA, moveSideB));
+                if(!d1 || !d2){
+                    autoMove = false;
+                    console.warn("NOW MANUALLY ENTER THE VALUES");
+                    continue;
+                }
+            }
+            i = gameController.move(d1!, d2!, thisPlayer, i);
         } catch (error) {
             // console.error(chalk.red(error));
             console.error(error);
             continue;
         }
-        // boxCreated ? null : i++;
     }
 
     console.log("***********CONGRATULATIONS***********");
@@ -109,3 +120,66 @@ try {
     console.error(error)
 }
 
+function testGame(startA: number, startB: number, boxDimension: number, moveSideA: number=0, moveSideB: number=1){
+    let d1 = startA + ""+startB;
+    // let moveSideA = 0;
+    // let moveSideB = 1;
+    if(d1=="0"+boxDimension){   // 04
+        moveSideA = 1;
+        moveSideB = 0;
+    }else if(d1==boxDimension+""+boxDimension){ // 44
+        moveSideA = 0;
+        moveSideB = -1;
+    }else if(d1==boxDimension+"0"){ // 40
+        moveSideA = -1;
+        moveSideB = 0;
+    }
+
+    if(d1=="1"+(boxDimension-1)){   // 13
+        moveSideA = 0;
+        moveSideB = -1;
+    }else if(d1==(boxDimension-1)+""+(boxDimension-1)){ // 33
+        moveSideA = -1;
+        moveSideB = 0;
+    }else if(d1==(boxDimension-1)+"0"){ // 30
+        moveSideA = 0;
+        moveSideB = 1;
+    }
+    let d2 = (startA+moveSideA) +"" + (startB+moveSideB);
+
+    console.log(d1+ "-->" + d2);
+    if(d1=="10"){
+        return {d1: null, d2: null, testPoint1: 1, testPoint2: 0, moveSideA, moveSideB}
+    }
+    // let d2 = startA<boxDimension? 
+    //                 startB<boxDimension?
+    //                     startA+""+(startB+moveSide)
+    //                     : (startA+moveSide) + ""+startB
+    //                 :
+    return {d1, d2, testPoint1: startA+moveSideA, testPoint2: startB+moveSideB, moveSideA, moveSideB};
+}
+
+function setGameParams(sizeOfBoard?: number, numberOfPlayers?: number, e?: any){
+    // try {
+        sizeOfBoard = prompt('How many rows-columns of boxes do you want in the game? ');
+        sizeOfBoard = Number(sizeOfBoard)
+        numberOfPlayers = prompt('How many players will play the game. Enter a number between 2 and 5?');
+        numberOfPlayers = Number(numberOfPlayers);
+        if(sizeOfBoard<2){
+            throw new Error('Size of board too small')
+        }
+        if(!isNaN(numberOfPlayers) && !isFinite(numberOfPlayers)){
+            e="Please enter a valid numeric value for number of players";
+            throw new Error(e)
+        }
+        if(numberOfPlayers<2 || numberOfPlayers>5){
+            e="Please enter a value between 2 and 5 for the number of players"
+            throw new Error(e)
+        }
+        e=null;
+        return {e, sizeOfBoard, numberOfPlayers}
+    // } catch (error) {
+    //     console.log(error)
+    //     e=error;
+    // }
+}
