ba4afd1c39eccc40c1b4c90e2c168625ca38be69
doctor-acid <avnishbadoni@gmail.com>
2023-11-01 13:02:59 +0530

    win strategy bug solved

--- src/Game/Strategies/winStrategyMaxBox.ts
+++ src/Game/Strategies/winStrategyMaxBox.ts
@@ -10,13 +10,16 @@ export default class MaxBoxWin implements winningStrategy{
         let dim = g.board?.boxDimension!
         let thresholdCount = Math.floor(dim*dim/2);
         for(const [key, box] of g.board?.boxMap!){
+            
             if(box.getOwner()){
-                playerBoxCount.set(box.getOwner()?.name!, playerBoxCount.get(box.getOwner()?.name!) ? 1 : playerBoxCount.get(box.getOwner()?.name!)! +1);
+                // console.log("owner based box")
+                playerBoxCount.set(box.getOwner()?.name!, playerBoxCount.get(box.getOwner()?.name!) ? playerBoxCount.get(box.getOwner()?.name!)! +1 : 1);
                 if(playerBoxCount.get(box.getOwner()?.name!)! > thresholdCount){
                     return box.getOwner();
                 }
             }
         }
+        console.log(playerBoxCount)
         return null;
     }
 }
